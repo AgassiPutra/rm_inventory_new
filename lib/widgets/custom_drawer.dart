55ec5f0cddbdffa380197451354bc69f50742f38
Agassi Putra <agassiputrakhautsar@gmail.com>
2025-09-01 11:09:29 +0700

    update versi 0.1.8

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -108,7 +108,7 @@ class _CustomDrawerState extends State<CustomDrawer> {
           Padding(
             padding: const EdgeInsets.only(bottom: 8.0),
             child: Text(
-              'Versi 0.1.7',
+              'Versi 0.1.8',
               style: TextStyle(fontSize: 12, color: Colors.grey),
             ),
           ),
